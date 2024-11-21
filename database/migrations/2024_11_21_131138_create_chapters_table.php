e1d08134eedb23e506de77a166b663e693686e2f
akankshaverm <priyaakanksha0507@gmail.com>
2024-11-21 20:09:52 +0530

    migration done

---
+++ database/migrations/2024_11_21_131138_create_chapters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('chapter_title');
+            $table->string('chapter_desc');
+            $table->foreignId('course_id')->constrained();
+            $table->string('chapter_slug')->unique();
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::prefix("user")->group(function(){
+
 });
+Route::prefix("admin")->group(function(){
+    Route::get("/",[AdminController::class,"dashboard"])->name("admin.dashboard");
+
+});
+
+
