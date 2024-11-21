439959b49860358de3724ed1909161553ffa4fad
akankshaverm <priyaakanksha0507@gmail.com>
2024-11-21 23:15:58 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/",[UserController::class,"index"])->name('homepage');
